898c8f48fe84dba094c2d5644531a2c273331079
Dennis Muturia <dmuturia@mo-de.com>
2018-11-19 17:01:25 +0300

    able to add the total. But if the array is a single unit

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,27 @@ import java.util.List;
 
 public class Main {
     //Parse in the array
-    static int arr[] = new int[]{3,1,2,0,4};
     public static  int waterStart(Integer[] n){
         List<Integer>diff = new ArrayList<>();
-        //get the first level
-        //loop through the array to get the start column
-        //Check if they are side by side
         int start=0;
         for (int i=0; i <n.length; i++){
-          if (n[i] >= 1){
+          if ((n[i] >= 1) && (n[i]>n[i + 1])){
               start = n[i];
               break;
           }
         }
-        //Find the highest peak of the array
+        //Finding out the start definately
         int max = (int) Collections.max(Arrays.asList(n));
-       // System.out.println(max);
-        //System.out.print(start);
-        //So now calculate the volume of water
+
 
         int difference = 0;//Initialize the volume of water
         for (int j = Arrays.asList(n).indexOf(start); j <= n.length; j++){
                 try {
-                   difference = max - n[j];
-                   diff.add(difference);
+                   difference = start - n[j];
+                   if (difference >= 0){
+                       diff.add(difference);
+                   }
+
                 }catch (IndexOutOfBoundsException e){
                     difference = 0;
                 }
@@ -47,7 +44,7 @@ public class Main {
 
 
     public static void main(String args[]){
-       Integer[] arr = {2,0,2,5,9};
+       Integer[] arr = {3, 0, 0, 2, 0, 4};
         waterStart(arr);
     }
 }
